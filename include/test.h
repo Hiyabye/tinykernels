acbe2b8ea6d6a07514e1113a6065adaa5f335056
Jihoon Lee <hiyabye0465@gmail.com>
2026-05-28 12:13:11 +0900

    Move test helpers into test.c

--- include/test.h
+++ include/test.h
@@ -1,20 +1,6 @@
 #ifndef TEST_H
 #define TEST_H
 
-#include "matrix.h"
-
-#include <stddef.h>
-
-#define COLOR_RED "\x1b[31m"
-#define COLOR_GREEN "\x1b[32m"
-#define COLOR_YELLOW "\x1b[33m"
-#define COLOR_BLUE "\x1b[34m"
-#define COLOR_MAGENTA "\x1b[35m"
-#define COLOR_CYAN "\x1b[36m"
-#define COLOR_RESET "\x1b[0m"
-#define CHECK_MARK "\u2713"
-
-int matrix_equal(const Matrix *a, const Matrix *b, double eps);
 void test_matmul_correctness(void);
 
 #endif // TEST_H
--- src/test.c
+++ src/test.c
@@ -2,7 +2,17 @@
 #include "matmul.h"
 #include "matrix.h"
 
+#include <stddef.h>
 #include <stdio.h>
+
+#define COLOR_RED "\x1b[31m"
+#define COLOR_GREEN "\x1b[32m"
+#define COLOR_YELLOW "\x1b[33m"
+#define COLOR_BLUE "\x1b[34m"
+#define COLOR_MAGENTA "\x1b[35m"
+#define COLOR_CYAN "\x1b[36m"
+#define COLOR_RESET "\x1b[0m"
+#define CHECK_MARK "\u2713"
 
 static void test_matrix_fill(Matrix *m) {
   for (size_t i = 0; i < m->rows; ++i) {
@@ -12,7 +22,7 @@ static void test_matrix_fill(Matrix *m) {
   }
 }
 
-int matrix_equal(const Matrix *a, const Matrix *b, double eps) {
+static int matrix_equal(const Matrix *a, const Matrix *b, double eps) {
   if (!a || !b || !a->data || !b->data) {
     fprintf(stderr, "invalid matrix\n");
     return 0;
